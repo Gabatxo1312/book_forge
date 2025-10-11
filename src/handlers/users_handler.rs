7ef956383fd761024b758ab6713eb23167696487
admin1312 <admin1312@kl.netlib.re>
2025-10-11 16:52:40 +0200

    Add users crud

--- src/handlers/users_handler.rs
+++ src/handlers/users_handler.rs
@@ -1,8 +1,8 @@
 use std::sync::Arc;
 
 use askama::Template;
-use sea_orm::{ EntityTrait, Set, ActiveModelTrait };
-use axum::{extract::State, http::{StatusCode}, response::{Html, Redirect}, routing::{get, post}, Form, Router};
+use sea_orm::{ ActiveModelTrait, DeleteResult, EntityTrait, Set };
+use axum::{extract::{Path, State}, http::StatusCode, response::{Html, Redirect}, routing::{get, post}, Form, Router};
 
 use crate::config::AppState;
 
@@ -12,6 +12,9 @@ pub fn routes() -> Router<Arc<AppState>> {
     Router::new()
         .route("/users", get(get_all_users))
         .route("/users", post(create_user))
+        .route("/users/:id/delete", post(delete_user))
+        .route("/users/:id/edit", get(edit_user))
+        .route("/users/:id", post(update_user))
 }
 
 #[derive(Template)]
@@ -28,10 +31,57 @@ async fn get_all_users(
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
 
     Ok(Html(
-        UsersIndexTemplate {
-            users: users
-        }.render().unwrap()
+            UsersIndexTemplate {
+                users: users
+            }.render().unwrap()
     ))
+}
+
+#[derive(Template)]
+#[template(path = "users/edit.html")]
+struct EditUserTemplate {
+    user: user::Model,
+}
+
+async fn edit_user(
+    State(state): State<Arc<AppConfig>>,
+    Path(id): Path<i32>
+) -> Result<Html<String>, StatusCode> {
+    let user: Option<user::Model> = user::Entity::find_by_id(id).one(&state.db)
+        .await
+        .map_err(|_| StatusCode::NOT_FOUND)?;
+
+    match user {
+        Some(user) => {
+            Ok(Html(
+                    EditUserTemplate {
+                        user: user
+                    }.render().unwrap()
+                )
+            )
+        },
+        None => Err(StatusCode::NOT_FOUND)
+    }
+}
+
+async fn update_user(
+    State(state): State<Arc<AppConfig>>,
+    Path(id): Path<i32>,
+    Form(payload): Form<user::Model>
+) -> Result<Redirect, StatusCode> {
+    let user_by_id = user::Entity::find_by_id(id).one(&state.db)
+        .await
+        .map_err(|_| StatusCode::NOT_FOUND)?;
+
+    let mut user_by_id: user::ActiveModel = user_by_id.unwrap().into();
+
+    user_by_id.name = Set(payload.name);
+
+    let _: user::Model = user_by_id.update(&state.db)
+        .await
+        .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    Ok(Redirect::to("/users"))
 }
 
 async fn create_user(
@@ -45,6 +95,18 @@ async fn create_user(
 
     new_user.insert(&state.db).await
         .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?;
+
+    Ok(Redirect::to("/users"))
+}
+
+async fn delete_user(
+    State(state): State<Arc<AppConfig>>,
+    Path(id): Path<i32>
+) -> Result<Redirect, StatusCode> {
+    let _: DeleteResult = user::Entity::delete_by_id(id)
+        .exec(&state.db)
+        .await
+        .map_err(|_| StatusCode::NOT_FOUND)?;
 
     Ok(Redirect::to("/users"))
 }
